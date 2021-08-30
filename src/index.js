e1cbe8806fb2c39d4c45c2c37357535953623b79
LeonID <lmatskevich79@gmail.com>
2021-08-30 21:35:52 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 function eval() {
     // Do not use eval!!!
-    
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let scor = 0;
+    expr = expr.split(' ').join('');
+
+    for(let i=0; i<expr.length; i++) {
+        if(expr[i] === '(') scor++;
+        if(expr[i] === ')') scor--;
+    }
+
+    if(scor != 0)  throw Error('ExpressionError: Brackets must be paired!');
+    if(expr.includes('/0')) throw Error('TypeError: Division by zero.');
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
